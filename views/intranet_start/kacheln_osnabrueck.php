a74d7c403b90db2023614415da1535f7a3d2c98f
asudau <sudau@elan-ev.de>
2019-06-14 14:07:21 +0200

    ewl #3

--- views/intranet_start/kacheln_osnabrueck.php
+++ views/intranet_start/kacheln_osnabrueck.php
@@ -4464,46 +4464,18 @@ a.th {
 
 
 </style>
-<style> 
-    #start_page #start_kurse .list_item .info { 
-        position: absolute; 
-        display: inline-block; 
-        left: 14px; bottom: 10px; 
-        color: #5ea2bb; 
-        background-clip: padding-box; 
-        background-image: none; 
-        padding: 6px 22px 10px 35px; 
-    } 
-    #start_page #start_kurse .list_item .infobild { 
-        width: 30px; 
-        height: 30px; 
-        float: left; 
-        margin: 24px 0px 10px 9px; 
-    } 
-    /* Tooltip text */ 
-    .info .tooltiptext { 
-        visibility: hidden; 
-        width: 220px; 
-        background-color: black; 
-        color: #fff; 
-        text-align: center; 
-        padding: 5px 0; 
-        border-radius: 6px; 
-        /* Position the tooltip text - see examples below! */ 
-        position: absolute; z-index: 1; 
-    } 
-    /* Show the tooltip text when you mouse over the tooltip container */ 
-    .info:hover .tooltiptext { 
-        visibility: visible; 
-    } 
-</style> 
+<style> #start_page #start_kurse .list_item .info { position: absolute; display: inline-block; left: 14px; bottom: 10px; color: #5ea2bb; background-clip: padding-box; background-image: none; padding: 6px 22px 10px 35px; } #start_page #start_kurse .list_item .infobild { width: 30px; height: 30px; float: left; margin: 24px 0px 10px 9px; } /* Tooltip text */ .info .tooltiptext { visibility: hidden; width: 220px; background-color: black; color: #fff; text-align: center; padding: 5px 0; border-radius: 6px; /* Position the tooltip text - see examples below! */ position: absolute; z-index: 1; } /* Show the tooltip text when you mouse over the tooltip container */ .info:hover .tooltiptext { visibility: visible; } </style> 
+<div id="start_page">  <div class="banner_inner"> 
+<div style="text-align: left"> </div>
 
-<div id="start_page">  
+</div>
 
 <section class="portal-boxen js-portalboxen">
 	<h2 class="kursportal-h2 align-center"></h2>
 	<div class="row">
 
+
+		
 
 <div class="columns small-12 medium-6 large-4 portal-boxenouter">
 			<div class="portal-boxeninner" style="background-image: url(<?=$plugin->getPluginURL().'/assets/images/business.jpg' ?>)" onclick="changeclass(this);">
@@ -4513,10 +4485,10 @@ a.th {
 					<p class="portal-boxendate"></p>
 				</div>
 				<div class="portal-boxenoverlay">
-					<h3 class="portal-boxenheadline">Kurse und Projekte</h3>
-					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p>Hier kommen Sie direkt zur umfassenden Übersicht über all Ihre Kurse, Projekte und Arbeitsgruppen.</p></div>
-					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/my_courses" type="submit" class="btn btn-primary portal-boxenbutton"><?=Icon::create('seminar', 'info_alt')?> weiter zur Übersicht <?=Icon::create('arr_1right', 'info_alt')?></a>
+					<h3 class="portal-boxenheadline">English For Your Studies B1</h3>
+					<p class="portal-boxendate">09.04.2018-11.06.2018</p>
+					<div class="portal-boxentext"><p>Unterstï¿½tzt Sie bei der gezielten Auffrischung und Erweiterung von Lese- und Schreibfï¿½higkeit in der englischen Sprache.<br>Dieser Kurs wird angeboten von der VHS Region Lï¿½neburg.</p></div>
+					<a href="https://vhs.lueneburg.de/kurse/kurssuche/kurs/Englisch-Online+English+For+Your+Studies+B1/nr/181-42000/bereich/details/#inhalt" type="submit" target='_blank' class="btn btn-primary portal-boxenbutton">weiter <img src="/assets/images/icons/white/arr_1right.svg"></a>
 					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
 				</div>
 			
@@ -4534,8 +4506,8 @@ a.th {
 				<div class="portal-boxenoverlay">
 					<h3 class="portal-boxenheadline">Tipp: Stud.IP Nachrichten</h3>
 					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p>Stud.IP bietet ein eigenes internes Nachrichtensystem. Interne Nachrichten werden Ihnen zusätzlich per Mail weitergeleitet. Dies können Sie optional im Bereich Profil/Einstellungen anpassen. </p></div>
-					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton"><?=Icon::create('mail', 'info_alt')?> weiter zu meinen Nachrichten <?=Icon::create('arr_1right', 'info_alt')?></a>
+					<div class="portal-boxentext"><p>Stud.IP bietet ein eigenes internes Nachrichtensystem. Interne Nachrichten werden Ihnen zusï¿½tzlich per Mail weitergeleitet. Dies kï¿½nnen Sie optional im Bereich Profil/Einstellungen anpassen. </p></div>
+					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton">weiter zu meinen Nachrichten <img src="http://localhost/studip3.4/public/assets/images/icons/lightblue/mail.svg"></a>
 					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
 				</div>
 			
@@ -4547,14 +4519,14 @@ a.th {
 			<div class="portal-boxeninner" style="background-image: url(<?=$plugin->getPluginURL().'/assets/images/people.jpg' ?>)" onclick="changeclass(this);">
 			
 				<div class="portal-boxenintro">
-					<h3 class="portal-boxenheadline">Stud.IP für DozentInnen</h3>
+					<h3 class="portal-boxenheadline">Stud.IP fï¿½r DozentInnen</h3>
 					<p class="portal-boxendate"></p>
 				</div>
 				<div class="portal-boxenoverlay">
-					<h3 class="portal-boxenheadline"></h3>
-					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p></p></div>
-					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton">weiter <?=Icon::create('arr_1right', 'info_alt')?></a>
+					<h3 class="portal-boxenheadline">Mathematik fï¿½r Wirtschaftswissenschaften</h3>
+					<p class="portal-boxendate">28.05.2018-22.06.2018</p>
+					<div class="portal-boxentext"><p>Jetzt wirds speziell! In diesem Kurs erwerben Sie gezielt mathematische Grundlagenkenntnisse fï¿½r ein wirtschaftswissenschaftliches Studium.</p></div>
+					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton">weiter <img src="<?=$plugin->getPluginURL().'/assets/images/icons/white/arr_1right.svg'?>"></a>
 					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
 				</div>
 			
@@ -4562,22 +4534,22 @@ a.th {
 		</div>
 		
     <div class="columns small-12 medium-6 large-4 portal-boxenouter">
-			<div class="portal-boxeninner" style="background-image: url(<?=$plugin->getPluginURL().'/assets/images/qualitaet.jpg' ?>)" onclick="changeclass(this);">
-			
-				<div class="portal-boxenintro">
-					<h3 class="portal-boxenheadline">Qualitätsmanagement</h3>
-					<p class="portal-boxendate"></p>
-				</div>
-				<div class="portal-boxenoverlay">
-					<h3 class="portal-boxenheadline">Informationenen und Materialien rund um QM</h3>
-					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p></p></div>
-					<a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton">weiter <?=Icon::create('arr_1right', 'info_alt')?></a>
-					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
-				</div>
-			
-			</div>
-		</div>
+        <div class="portal-boxeninner" style="background-image: url(http://localhost/studip3.4/public/plugins_packages/asudau@elan-ev.de/VHSViewPlugin/images/banner.jpg)" onclick="changeclass(this);">
+
+            <div class="portal-boxenintro">
+                <h3 class="portal-boxenheadline">Die VHS Osnabrï¿½ck bietet ihren Dozentinnen und Dozenten regelmï¿½ï¿½ig die Gelegenheit die Funktionen und Einsatzmï¿½glichkeiten von Stud.IP in einer Schulung vor Ort kennenzulernen. Die nï¿½chste Schulung findet am 13.02.2019 statt. Zusï¿½tzlich stellen wir Ihnen verschiedene Vidoes und Anleitungen bereit um Stud.IP selbst zu erkunden. Viel Spaï¿½!</h3>
+                <p class="portal-boxendate"></p>
+            </div>
+            <div class="portal-boxenoverlay">
+                <h3 class="portal-boxenheadline">Vom Beruf ins Studium</h3>
+                <p class="portal-boxendate">20.08.2018-24.09.2018</p>
+                <div class="portal-boxentext"><p>Sie spielen mit dem Gedanken, zu studieren? In diesem Orientierungskurs finden Sie Wege an die Hochschule und Unterstï¿½tzung bei der Entscheidung fï¿½r oder gegen ein Studium.</p></div>
+                <a href="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>dispatch.php/messages/overview" type="submit" class="btn btn-primary portal-boxenbutton">weiter <img src="/assets/images/icons/white/arr_1right.svg"></a>
+                <button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
+            </div>
+
+        </div>
+    </div>        
 
 
         
@@ -4592,10 +4564,10 @@ a.th {
 					<p class="portal-boxendate"></p>
 				</div>
 				<div class="portal-boxenoverlay">
-					<h3 class="portal-boxenheadline">FAQ</h3>
-					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p>Wer legt die Kurse und Accounts für Nutzerinnen und Nutzer an? Woher kommt eigentlich Stud.IP und wer entwickelt es weiter? Antworten auf diese und andere Fragen finden Sie hier</p></div>
-					<a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <?=Icon::create('arr_1right', 'info_alt')?></a>
+					<h3 class="portal-boxenheadline">Zeit- und Selbstmanagement</h3>
+					<p class="portal-boxendate">13.08.2018-14.09.2018</p>
+					<div class="portal-boxentext"><p>Auch Erholung muss sein! Ein Studium und parallele Verpflichtungen erfordern eine gute Organisation. Lernen Sie die passenden Techniken kennen.</p></div>
+					<a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <img src="/assets/images/icons/white/arr_1right.svg"></a>
 					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
 				</div>
 			
@@ -4611,10 +4583,10 @@ a.th {
 					<p class="portal-boxendate"></p>
 				</div>
 				<div class="portal-boxenoverlay">
-					<h3 class="portal-boxenheadline">Stud.IP erkunden</h3>
-					<p class="portal-boxendate"></p>
-					<div class="portal-boxentext"><p>Lernen Sie die vielfältigen Möglichkeiten von Stud.IP mit Hilfe von Schritt für Schritt Anleitungen und Video-Tutorials kennen</p></div>
-					<a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <?=Icon::create('arr_1right', 'info_alt')?></a>
+					<h3 class="portal-boxenheadline">HANDWERKSZEUG studieren</h3>
+					<p class="portal-boxendate">10.09.2018-14.10.2018</p>
+					<div class="portal-boxentext"><p>Keine Angst vor Hausarbeiten und Referaten! In dem Kurs ï¿½HANDWERKSZEUG studierenï¿½ machen Sie sich mit den wichtigsten Spielregeln vertraut.</p></div>
+					<a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <img src="/assets/images/icons/white/arr_1right.svg"></a>
 					<button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
 				</div>
 			
@@ -4622,17 +4594,17 @@ a.th {
         </div>
 
     <div class="columns small-12 medium-6 large-4 portal-boxenouter">
-        <div class="portal-boxeninner" onclick="changeclass(this);">
+        <div class="portal-boxeninner" style="background-image: url(http://localhost/studip3.4/public/plugins_packages/asudau@elan-ev.de/VHSViewPlugin/images/banner.jpg)" onclick="changeclass(this);">
 
             <div class="portal-boxenintro">
-                <h3 class="portal-boxenheadline">Bei Fragen und Wünschen rund um Stud.IP helfen wir Ihnen gerne weiter:</h3>
+                <h3 class="portal-boxenheadline">Bei Fragen und Wï¿½nschen rund um Stud.IP helfen wir Ihnen gerne weiter:</h3>
                 <p class="portal-boxendate">Albrechtk@osnabrueck.de</p>
             </div>
             <div class="portal-boxenoverlay">
-                <h3 class="portal-boxenheadline"></h3>
-                <p class="portal-boxendate"></p>
-                <div class="portal-boxentext"><p></p></div>
-                <a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <?=Icon::create('arr_1right', 'info_alt')?></a>
+                <h3 class="portal-boxenheadline">Vom Beruf ins Studium</h3>
+                <p class="portal-boxendate">20.08.2018-24.09.2018</p>
+                <div class="portal-boxentext"><p>Sie spielen mit dem Gedanken, zu studieren? In diesem Orientierungskurs finden Sie Wege an die Hochschule und Unterstï¿½tzung bei der Entscheidung fï¿½r oder gegen ein Studium.</p></div>
+                <a href="" type="submit" class="btn btn-primary portal-boxenbutton">weiter <img src="/assets/images/icons/white/arr_1right.svg"></a>
                 <button class="portal-boxenclose" onclick="changeBack(this);"><img src="<?=$plugin->getPluginURL()?>/assets/images/close.png" alt="[X]"></button>
             </div>
 
