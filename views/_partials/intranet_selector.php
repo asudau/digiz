9792b1574eaee59aa008cb56140ef106b4aa8c85
asudau <sudau@elan-ev.de>
2019-07-01 13:47:54 +0200

    fix

--- views/_partials/intranet_selector.php
+++ views/_partials/intranet_selector.php
@@ -2,7 +2,7 @@
 <select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
 <option value="">Wählen Sie den gewünschten Startbereich...</option>
     <? foreach ($intranets as $intranet) : ?>
-        <option value="<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>plugins.php/studip_vhs/intranet_start/index/<?=$intranet?>"><?=Institute::find($intranet)->Name?></option>
+        <option value="<?=$this->controller->url_for('intranet_start/index/cid=' . $intranet) ?>"><?=Institute::find($intranet)->Name?></option>
     <? endforeach ?>
 </select> 
 
