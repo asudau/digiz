02de15bde4795fb9c0b6ae125368930d21b8a71e
asudau <sudau@elan-ev.de>
2019-07-01 13:49:04 +0200

    jetzt aber

--- views/_partials/intranet_selector.php
+++ views/_partials/intranet_selector.php
@@ -2,7 +2,7 @@
 <select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
 <option value="">Wählen Sie den gewünschten Startbereich...</option>
     <? foreach ($intranets as $intranet) : ?>
-        <option value="<?=$this->controller->url_for('intranet_start/index/cid=' . $intranet) ?>"><?=Institute::find($intranet)->Name?></option>
+        <option value="<?=$this->controller->url_for('intranet_start/index?cid=' . $intranet) ?>"><?=Institute::find($intranet)->Name?></option>
     <? endforeach ?>
 </select> 
 
